251532cb3fefe98e7c33e47b6e0bf614ef8347a1
Qiqi Zhao <qiqzhao@thoughtworks.com>
2017-09-21 10:37:40 +0800

    calculate string length & sort

--- src/main/java/com/thoughtworks/star/Cal.java
+++ src/main/java/com/thoughtworks/star/Cal.java
@@ -1,9 +1,18 @@
 package com.thoughtworks.star;
 
-import java.util.List;
+import java.util.*;
 
 public class Cal {
     public int getEvenSum(List<Integer> list) {
         return list.stream().filter(item -> item % 2 == 0).reduce(0, (a, b) -> a + b);
+    }
+
+    public Map<String, Integer> sort(List<String> list) {
+        Collections.sort(list);
+        Map<String, Integer> map = new LinkedHashMap<>();
+        for (String str : list) {
+            map.put(str, str.length());
+        }
+        return map;
     }
 }
--- src/test/java/com/thoughtworks/star/CalTest.java
+++ src/test/java/com/thoughtworks/star/CalTest.java
@@ -2,9 +2,9 @@ package com.thoughtworks.star;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -13,6 +13,18 @@ public class CalTest {
     public void should_return_all_even_sum() {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         assertThat(new Cal().getEvenSum(list), is(30));
+
+    }
+
+    @Test
+    public void should_return_sorted_string_with_length() {
+        List<String> list = Arrays.asList("ynpan", "yzqi", "ybowang", "qiqzhao", "yibtan", "abc", "sjyuan");
+
+        Map<String, Integer> resultMap = new LinkedHashMap<>();
+        resultMap = new Cal().sort(list);
+
+        assertThat(resultMap.keySet(), contains("abc", "qiqzhao", "sjyuan", "ybowang", "yibtan", "ynpan", "yzqi"));
+        assertThat(resultMap.values(), contains(3, 7, 6, 7, 6, 5, 4));
 
     }
 }
