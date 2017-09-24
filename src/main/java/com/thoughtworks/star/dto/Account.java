1c5314cf052a6618399859f59fff8a6705778545
Qiqi Zhao <qiqzhao@thoughtworks.com>
2017-09-24 20:12:56 +0800

    Add api test for login controller

--- src/main/java/com/thoughtworks/star/dto/Account.java
+++ src/main/java/com/thoughtworks/star/dto/Account.java
@@ -17,7 +17,21 @@ public class Account {
         return age;
     }
 
+    public Account setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public Account setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public static Account builder() {
+        return new Account();
     }
 }
